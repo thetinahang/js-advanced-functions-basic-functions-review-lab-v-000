cead56382af1e318c2e6dc182082b969931ef5fc
Tina Hang <mstinahang@gmail.com>
2020-02-02 04:23:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Your code here
 function saturdayFun(activity="roller-skate") {
+<<<<<<< HEAD
   return `This Saturday, I want to ${activity}!`
+=======
+  return `This Saturday, I want to ${activity}!`;
+>>>>>>> 11ff5c309ccd12960e34cd63cfb1c166c85d6657
 }
