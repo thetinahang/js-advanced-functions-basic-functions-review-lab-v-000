b529db9189107dfd96ed3cf945eb467d76e963ca
Tina Hang <mstinahang@gmail.com>
2020-02-02 04:41:27 +0000

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,36 @@
 function saturdayFun(activity="roller-skate") {
   return `This Saturday, I want to ${activity}!`
 }
+
+function mondayWork(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(symbol="*") {
+  return function(adjective) {
+    return `You are ${symbol}${adjective}${symbol}!`
+  }
+}
+
+const Calculator = {
+  add: function(a,b) {
+    return a + b
+  },
+  subtract: function(a,b) {
+    return a - b
+  },
+  multiply: function(a,b) {
+    return a * b
+  },
+  divide: function(a,b) {
+    return a / b
+  }
+}
+
+function actionApplyer(integer, array) {
+  let start = integer;
+  for (let i = 0; i < array.length; i++ ) {
+     start = array[i](start)
+  }
+  return start
+}
